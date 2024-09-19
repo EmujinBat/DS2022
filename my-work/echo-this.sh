46dee6de7962e628f42b345acffbb1c66b4228a9
EmujinBat <kfm8nx@virginia.edu>
2024-09-19 10:54:56 -0400

    class practice scripting

---
+++ my-work/echo-this.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Echo out something
+
+VAR1="Salted Caramel"
+
+echo $VAR1
+
+# $ is important because it tells it to not assign anything else
+
+echo "Hey $LOGNAME I see that your favorite flavor of ice cream is $VAR1"
+
